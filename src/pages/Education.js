af8b8b0eac585204ca2c2a88a2356b2eb460e894
Yaowen Xu <x@Macintosh.local>
2022-09-07 13:04:22 -0700

    -

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -10,48 +10,48 @@ const Education = () => (
     <div className='text-box'>
       <h1>University of Califonia, Berkeley</h1>
       <h2>Bachelor of Arts in Computer Science</h2>
-      <p>Aug. 2018 - Dec. 2021</p>
+      {/* <p>Aug. 2018 - Dec. 2021</p> */}
       <ul>
         <li
-          onClick={() =>
-            window.open('https://inst.eecs.berkeley.edu/~cs61a/fa18/', '_blank')
-          }
+        // onClick={() =>
+        //   window.open('https://inst.eecs.berkeley.edu/~cs61a/fa18/', '_blank')
+        // }
         >
           Structure and Interpretation of Computer Programs
         </li>
         <li
-          onClick={() => window.open('https://sp19.datastructur.es/', '_blank')}
+        // onClick={() => window.open('https://sp19.datastructur.es/', '_blank')}
         >
           Data Structures
         </li>
         <li
-          onClick={() =>
-            window.open('https://inst.eecs.berkeley.edu/~cs170/fa20/', '_blank')
-          }
+        // onClick={() =>
+        //   window.open('https://inst.eecs.berkeley.edu/~cs170/fa20/', '_blank')
+        // }
         >
           Efficient Algorithms and Intractable Problems
         </li>
         <li
-          onClick={() =>
-            window.open('https://www.cs160summer2021.com/', '_blank')
-          }
+        // onClick={() =>
+        //   window.open('https://www.cs160summer2021.com/', '_blank')
+        // }
         >
           User Interface Design and Development
         </li>
         <li
-          onClick={() =>
-            window.open(
-              'https://www2.eecs.berkeley.edu/Courses/CS169/',
-              '_blank'
-            )
-          }
+        // onClick={() =>
+        //   window.open(
+        //     'https://www2.eecs.berkeley.edu/Courses/CS169/',
+        //     '_blank'
+        //   )
+        // }
         >
           Software Engineering
         </li>
         <li
-          onClick={() =>
-            window.open('https://cs186berkeley.net/fa21/', '_blank')
-          }
+        // onClick={() =>
+        //   window.open('https://cs186berkeley.net/fa21/', '_blank')
+        // }
         >
           Introduction to Database Systems
         </li>
